07412727962521a72518a41c23bdcd6d4f0b73c1
kondratyuk91 <kondratyuk91@gmail.com>
2019-05-08 17:55:11 +0300

    Add files via upload

--- STM32F103C8T6_CMSIS_FreeRTOS/workspace/Code/inc/sysInit.h
+++ STM32F103C8T6_CMSIS_FreeRTOS/workspace/Code/inc/sysInit.h
@@ -2,9 +2,20 @@
 
 #include "stm32f10x.h"
 
+/*************************************************** Defines ***************************************************/
+
+#define INT_FLASH_KEY1				((uint32_t)0x45670123)
+#define INT_FLASH_KEY2				((uint32_t)0xCDEF89AB)
+
 /************************************************ Use functions ************************************************/
 
-void RCCInit(void);
+void RCCInit (void);
 void GenMCO (void);
-void GPIOInit(void);
+void GPIOInit (void);
 
+void flashUnlock (void);
+void flashlock (void);
+void flashEraseAll (void);
+
+uint32_t flashReadData (uint32_t address);
+void flashWriteData(uint32_t address, uint32_t data);
